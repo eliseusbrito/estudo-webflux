b3a699e3b7f9840ad2fd3597edbf184be5e762da
eliseusbrito <eliseusbrito@gmail.com>
2023-06-04 09:03:16 -0300

    Projeto criado em spring start io

---
+++ src/test/java/br/com/eliseubrito/estudowebflux/EstudoWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eliseubrito.estudowebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
